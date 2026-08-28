9f77e030fded7b153718567153d88a00998082e0
ijsbol <hi@uwu.gal>
2026-08-29 01:15:52 +0200

    use disk directly for static files

--- infra/abigail.sh
+++ infra/abigail.sh
@@ -11,6 +11,23 @@ server {
     client_header_buffer_size 4k;
     large_client_header_buffers 4 16k;
 
+    gzip on;
+    gzip_types text/css application/javascript application/json image/svg+xml application/font-woff font/woff2;
+    gzip_min_length 256;
+
+    location /static/ {
+        alias /opt/websites/abigail.sh/_served/static/;
+        access_log off;
+        tcp_nodelay off;
+        sendfile on;
+        location ~* \.(css|js|woff2?|ttf|otf)$ {
+            add_header Cache-Control "public, max-age=31536000, immutable";
+        }
+        location ~* \.(avif|png|jpe?g|gif|svg|webp|json)$ {
+            add_header Cache-Control "public, max-age=86400";
+        }
+    }
+
     location / {
         proxy_pass http://127.0.0.1:7867;
         proxy_http_version 1.1;
